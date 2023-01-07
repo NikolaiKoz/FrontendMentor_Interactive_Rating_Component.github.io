2806996a3315da3d8c4a182cb2d7ee363a8047e4
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-07 10:30:03 -0300

    docs: :memo: cleanContainer.js, DOCS.
    All the docs about this function was added.

--- src/js/Components/Container/cleanContainer.js
+++ src/js/Components/Container/cleanContainer.js
@@ -1,10 +1,23 @@
+/**
+ * @author: Vallejos Jose Luis.
+ * @function: cleanContainer
+ * @name: cleanContainer
+ * @description: This function aplly the class 'isDesappear' to the container and then remove the innerHTML of the container.
+ * @param: none
+ * @return: none
+ */
+
 const cleanContainer = () => {
+  /**
+   * @name: container
+   * @description: This variable is the Container of the card.
+   * @type: HTMLElement
+   */
+  const container = document.getElementsByClassName("Container")[0];
 
-    document.getElementsByClassName('Container')[0].classList.add('isDesappear');
+  container.classList.add("isDesappear");
 
-    setTimeout(() => {
-        document.getElementsByClassName('Container')[0].innerHTML = '';
-    }, 500);
-
-
+  setTimeout(() => {
+    container.innerHTML = "";
+  }, 500);
 };
