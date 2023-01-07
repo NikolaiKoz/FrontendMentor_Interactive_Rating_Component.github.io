2d98ba419cf53dfc68e49919f102ba3a3ba549e2
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-07 09:31:07 -0300

    style: :art: Time of transitions, EDIT.
    we have more time of animation, .2s to .5s

--- src/js/Components/Container/choiseOption.js
+++ src/js/Components/Container/choiseOption.js
@@ -1,5 +1,19 @@
+/**
+ * @author: Vallejos Jose Luis.
+ * @function: choiseOption
+ * @name: choiseOption
+ * @description: This function first remove the class 'isActive' from all the buttons and then add the class 'isActive' to the button that was clicked.
+ * @param: none
+ * @return: none
+ */
+
 const choiseOption = () => {
 
+    /**
+     * @name: allBtn
+     * @description: This variable is a NodeList of all the buttons.
+     * @type: NodeList
+     */
     const allBtn = document.querySelectorAll('.Container-ratio-btn');
 
     allBtn.forEach((btn) => {
